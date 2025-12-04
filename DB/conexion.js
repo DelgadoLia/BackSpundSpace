43bb2224fc2314fc351f3e9a74e1835d83b48d36
Elia Arteaga Delgado <delgadolia76@gmail.com>
2025-12-04 17:51:55 -0600

    Rename main page to index.html

--- DB/conexion.js
+++ DB/conexion.js
@@ -1,14 +1,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config(); 
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0 
-});
+const connectionString = process.env.DATABASE_URL + '?ssl=true';
+const pool = mysql.createPool(connectionString);
 
-module.exports = pool; 
+module.exports = pool;
